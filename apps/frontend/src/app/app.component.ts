86e7e224777b506e116b9e3935973f5eadb444e3
msieben <marco.sieben@andrena.de>
2024-10-01 17:14:47 +0200

    Generate frontend api, adapt code

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,24 +1,21 @@
-import { Component, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { User } from '../models/user';
-import { AsyncPipe } from '@angular/common';
-import { shareReplay } from 'rxjs';
-import { environment } from '../environments/environment';
+import { Component, inject } from '@angular/core'
+import { AsyncPipe } from '@angular/common'
+import { shareReplay } from 'rxjs'
+import { UserControllerService } from './generated/openapi'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [AsyncPipe],
-  template: `
-    @if (user$ | async; as user) {
-    <h1>Welcome, {{ user.userName }}</h1>
-    }
-  `,
+    selector: 'app-root',
+    standalone: true,
+    imports: [AsyncPipe],
+    template: `
+      @if (user$ | async; as user) {
+        <h1>Welcome, {{ user.userName }}</h1>
+      }
+    `,
 })
 export class AppComponent {
-  private httpClient = inject(HttpClient);
+    private userControllerService = inject(UserControllerService)
 
-  user$ = this.httpClient
-    .get<User>(`${environment.baseUrl}/api/user/current`)
-    .pipe(shareReplay({ bufferSize: 1, refCount: true }));
+    user$ = this.userControllerService.getCurrentUser()
+        .pipe(shareReplay({ bufferSize: 1, refCount: true }))
 }
--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BASE_PATH } from './generated/openapi'
+import { environment } from '../environments/environment'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
+    {provide: BASE_PATH, useValue: environment.baseUrl}
   ],
 };
