71c2a0e952bf05abd99f9aa4cd65a332bb5bcd73
msieben <marco.sieben@andrena.de>
2024-10-01 17:14:19 +0200

    Generate frontend api, adapt code

--- apps/backend/build.gradle.kts
+++ apps/backend/build.gradle.kts
@@ -1,42 +1,42 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
-	id("maven-publish")
-  id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
+    java
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("maven-publish")
+    id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
 }
 
 group = "de.andrena"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
-  implementation("org.springframework.boot:spring-boot-starter-web")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 springBoot {
-	buildInfo()
+    buildInfo()
 }
 publishing {
-	publications {
-		create<MavenPublication>("mavenJava") {
-			artifact(tasks.getByName("bootJar"))
-		}
-	}
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifact(tasks.getByName("bootJar"))
+        }
+    }
 }
